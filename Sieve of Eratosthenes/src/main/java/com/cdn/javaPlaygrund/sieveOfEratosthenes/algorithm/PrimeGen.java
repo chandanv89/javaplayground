9679165cff221bcbab89d8bee6582c157aa244b2
Chandan Veerabhadrappa <chandanv89@gmail.com>
2017-11-15 23:00:41 +0000

    Implemented SoE imperically

--- Sieve of Eratosthenes/src/main/java/com/cdn/javaPlaygrund/sieveOfEratosthenes/algorithm/PrimeGen.java
+++ Sieve of Eratosthenes/src/main/java/com/cdn/javaPlaygrund/sieveOfEratosthenes/algorithm/PrimeGen.java
@@ -3,24 +3,36 @@ package com.cdn.javaPlaygrund.sieveOfEratosthenes.algorithm;
 import com.cdn.javaPlaygrund.sieveOfEratosthenes.PrimeGenerationAlgorithm;
 
 import static java.lang.Math.sqrt;
+import static java.util.Arrays.fill;
 
 public class PrimeGen {
 
-    public int[] generatePrime(int to){//, PrimeGenerationAlgorithm algorithm) {
-        //if (from < 2) return new int[]{from};
+    public int[] generatePrime(int n, PrimeGenerationAlgorithm algorithm) {
+        if (n < 2) return new int[]{};
+        if (n == 2) return new int[]{2};
 
-        if(to<3) return new int[]{to};
+        if (algorithm.equals(PrimeGenerationAlgorithm.SIEVE_OF_ERATOSTHENES))
+            return sieveOfEratosthenes(n);
 
-        boolean[] primeTable = new boolean[to];
-        for (int i = 0; i < to; i++) {
-            primeTable[i] = true;
+        return new int[]{};
+    }
+
+    private int[] sieveOfEratosthenes(int n) {
+        boolean[] primeTable = new boolean[n + 1];
+        fill(primeTable, true);
+
+        int N = (int) sqrt(n);
+        for (int i = 2; i <= N; i++) {
+            if (primeTable[i])
+                for (int j = i * 2; j <= n; j += i) {
+                    primeTable[j] = false;
+                }
         }
 
-        int n = (int) sqrt(to);
-        for(int i=2; i<=n; i++){
-            for(int j=i*2; j<to; j++){
-                if(primeTable[j]) primeTable[j]=false;
-            }
-        }
+        int[] primes = new int[N*N];
+        for (int i = 2, j = 0; i <= n; i++)
+            if (primeTable[i]) primes[j++] = i;
+
+        return primes;
     }
 }
